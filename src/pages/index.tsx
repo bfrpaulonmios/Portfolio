b5abac3659cae0faa0629c217ecac6f8679f8972
Bruno Paulon <122818137+Bfrpaulon@users.noreply.github.com>
2023-06-06 13:36:38 +0000

    update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar />
       <main className="text-primary-100 bg-text-100">Modern portfolio project</main>
     </>
   );
