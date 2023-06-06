3465be3de8d525eef555ee40f03cbe84a564ec95
Bruno Paulon <122818137+Bfrpaulon@users.noreply.github.com>
2023-06-06 13:56:46 +0000

    .

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-    return <nav>
-        <div>
-
-        <div/>
-        <nav/>
-}
-
+  return (
+    <nav className="flex justify-between items">
+      <div>
+        <div />
+        <span>Portfolio</span>
+      </div>
+      <div>
+        <Link href="#">Home</Link>
+        <Link href="#">Projects</Link>
+        <Link href="#">Contact</Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
