701b85286f22beecf7c471bc6650a52b0829161e
Bruno Paulon <122818137+Bfrpaulon@users.noreply.github.com>
2023-06-06 22:56:15 -0300

    update HeroSection.tsx and Navbar.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,14 +1,14 @@
 import { motion } from "framer-motion";
 
 const technologies = [
-    { name: "React JS", icon: "https://gcdnb.pbrd.co/images/BVkAXhWCrJNe.png?o=1" },
-    { name: "Next JS", icon: "/path/to/tailwind-icon.png" },
-    { name: "JavaScript", icon: "/path/to/framer-icon.png" },
-    { name: "TypeScript", icon: "/path/to/nextjs-icon.png" },
-    { name: "TailWind CSS", icon: "/path/to/other-icon.png" },
-    { name: "Frame Motion", icon: "https://github.com/Bfrpaulon/Portfolio/blob/main/icons/frame.png" },
-    { name: "Node JS", icon: "/path/to/other-icon.png" },
-    { name: "Mongo DB", icon: "/path/to/other-icon.png" },
+    { name: "React JS" },
+    { name: "Next JS" },
+    { name: "JavaScript" },
+    { name: "TypeScript" },
+    { name: "TailWind CSS" },
+    { name: "Frame Motion" },
+    { name: "Node JS" },
+    { name: "Mongo DB" },
 ];
 
 const HeroSection = () => {
@@ -24,6 +24,56 @@ const HeroSection = () => {
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ duration: 0.5 }}
                     />
+                    <motion.div
+                        className="flex items-end"
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5, delay: 0.4 }}
+                    >
+
+                        <div className="flex ml-8 space-x-6 mt-5 items-center">
+                            <motion.a
+                                href="https://drive.google.com/file/d/17sdQTGdlNcb8fC1rOc17KUIAJvJILmvj/view"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                whileHover={{ scale: 1.2 }}
+                             >
+                                <img src="https://gcdnb.pbrd.co/images/LLXh5PEPr3WK.png?o=1" alt="Resume" className="w-14 h-12 " />
+                            </motion.a>
+                            <motion.a
+                                href="https:/www.linkedin.com/in/bfrpaulon"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                whileHover={{ scale: 1.2 }}
+                            >
+                                <img src="https://gcdnb.pbrd.co/images/d1O0FgSl8N4B.png?o=1" alt="LinkedIn" className="w-6 h-6 " />
+                            </motion.a>
+                            <motion.a
+                                href="https:/www.github.com/bfrpaulon"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                whileHover={{ scale: 1.2 }}
+                            >
+                                <img src="https://gcdnb.pbrd.co/images/hxdnD7KF64jq.png?o=1" alt="Github" className="w-6 h-6" />
+                            </motion.a>
+                            <motion.a
+                                href="https://www.instagram.com/bfrpaulondev/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                whileHover={{ scale: 1.2 }}
+                            >
+                                <img src="https://gcdnb.pbrd.co/images/UiKpFCYfLq09.png?o=1" alt="Instagram" className="w-6 h-6" />
+                            </motion.a>
+                            <motion.a
+                                href="https://www.youtube.com/channel/UCEJiIGAkQ5_YcdsRZjGJwjw"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                whileHover={{ scale: 1.2 }}
+                            >
+                                <img src="https://gcdnb.pbrd.co/images/QyQeL5HGFoCc.png?o=1" alt="Youtube" className="w-6 h-6" />
+                            </motion.a>
+                        </div>
+                    </motion.div>
                 </div>
                 <div className="md:w-1/2 mx-9">
                     <motion.div
@@ -32,9 +82,12 @@ const HeroSection = () => {
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 0.5 }}
                     >
-                        <p className="text-4xl text-bg-100 py-4 text-center">Hello World !</p>
+                        <p className="text-4xl text-text-200 py-4 text-center"> # Hello World #</p>
                         <p className="text-lg text-text-200 text-justify">
-                            <strong className="text-bg-100 ">  Hello World, my name is Bruno Paulon. </strong> I'm a front-end developer with a passion for technology. With more than 4 years of experience in front-end projects from conception to implementation. My goal is to create a beautiful and functional user interface. I am always open to new opportunities and challenges. Discover the power of a seamless user interface. Contact me and let's turn your ideas into reality!                        </p>
+                            <strong className="text-bg-100 ">  Hey folks! My name is Bruno Paulon. </strong> I am a front-end developer with a passion for technology. With more than 4 years of experience in front-end projects from conception to implementation. My goal is to create a beautiful and functional user interface. I am always open to new opportunities and challenges. Discover the power of a seamless user interface. Contact me and lets turn your ideas into reality!</p>
+                        <button className="bg-accent-100 text-white px-4 py-2 rounded mt-5 hover:bg-accent-200">
+                            Contact Me →
+                        </button>
                     </motion.div>
                     <motion.div
                         className="mb-6"
@@ -50,33 +103,12 @@ const HeroSection = () => {
                                     className="bg-bg-200 p-4 rounded-lg flex items-center justify-center"
                                     whileHover={{ scale: 1.05 }}
                                 >
-                                    <img src={tech.icon} alt={tech.name} className="w-12 h-12" />
                                     <p className="text-text-100 ml-2">{tech.name}</p>
                                 </motion.div>
                             ))}
                         </div>
                     </motion.div>
-                    <motion.div
-                        className="flex items-center"
-                        initial={{ opacity: 0, x: -100 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.5, delay: 0.4 }}
-                    >
-                        <button className="bg-accent-100 text-white px-4 py-2 rounded mr-4">
-                            Entre em Contato
-                        </button>
-                        <div className="flex space-x-2">
-                            <motion.a
-                                href="https://link-da-sua-rede-social"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                whileHover={{ scale: 1.2 }}
-                            >
-                                <img src="/path/to/your/social-media-icon.png" alt="Social Media" className="w-6 h-6" />
-                            </motion.a>
-                            {/* Adicione mais links para outras redes sociais */}
-                        </div>
-                    </motion.div>
+
                 </div>
             </div>
         </div>
