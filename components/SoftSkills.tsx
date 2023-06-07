7db9bd673fb93a070a09ebe4048890be0b9d3852
brunopaulon2021 <79466541+brunopaulon2021@users.noreply.github.com>
2023-06-07 22:02:27 +0100

    update SoftSkills.tsx and tsconfig.json

--- components/SoftSkills.tsx
+++ components/SoftSkills.tsx
@@ -8,13 +8,85 @@ const skillsData = [
         name: "HTML",
         icon: "/path/to/html-icon.png",
         phrase: "HTML and me? We are like 'open' and 'close' tags, always together!",
-        startYear: 2010,
+        startYear: 2020,
     },
     {
         name: "CSS",
         icon: "/path/to/css-icon.png",
         phrase: "CSS is my superpower!",
-        startYear: 2012,
+        startYear: 2020,
+    },
+    {
+        name: "Javascript",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2020,
+    },
+    {
+        name: "Typescript",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "Bootstrap",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "SASS/LESS/STYLUS",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "UI/UX",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "React JS",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "Material UI",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2021,
+    },
+    {
+        name: "Next JS",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2022,
+    },
+    {
+        name: "Node JS",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2022,
+    },
+    {
+        name: "Mongo DB",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2022,
+    },
+    {
+        name: "Chakra UI",
+        icon: "/path/to/css-icon.png",
+        phrase: "CSS is my superpower!",
+        startYear: 2022,
     },
     // Add more technologies as needed
 ];
@@ -50,12 +122,19 @@ const SoftSkills = () => {
     }, []);
 
     const calculateExperienceInYears = (startYear: number) => {
-        const experienceInYears = currentYear - startYear;
-        return experienceInYears;
+        if (startYear < 1) {
+            const currentDate = new Date();
+            const startDate = new Date(currentDate.getFullYear(), 0, 1);
+            const daysDifference = Math.ceil((currentDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
+            return `${daysDifference} days`;
+        } else {
+            const experienceInYears = currentYear - startYear;
+            return `${experienceInYears} years`;
+        }
     };
 
     return (
-        <div id="softSkillsSection" className="flex justify-center items-center h-screen">
+        <div id="softSkillsSection" className="flex justify-center items-center h-screen"> 
             <div className="max-w-screen-lg w-full mx-auto px-4">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                     {skillsData.map((skill, index) => (
@@ -81,8 +160,7 @@ const SoftSkills = () => {
                                 </div>
                                 <div>
                                     <h3 className="text-lg font-semibold text-text-100">{skill.name}</h3>
-                                    <p className="text-sm text-text-200">{`${calculateExperienceInYears(skill.startYear)} years of experience`}</p>
-                                    <p className="text-sm text-text-200">{skill.phrase}</p>
+                                    <p className="text-sm text-text-200">{`${calculateExperienceInYears(skill.startYear)} of experience`}</p>
                                 </div>
                             </motion.div>
                         </Link>
